675d31a5bef1d33d35be50b2332fc8df3d6e5eb1
Milo Ramirez <sinkpig00@gmail.com>
2022-10-18 21:24:48 -0300

    Bundler fix

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -14,9 +14,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to lists_path, status: :see_other
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
