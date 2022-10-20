dc51569ccc2dbf59036705bd8b5eed5c46407c30
Milo Ramirez <sinkpig00@gmail.com>
2022-10-19 21:35:08 -0300

    Added reviews fixed css

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,12 @@
 class ListsController < ApplicationController
+  before_action :find, except: %i[index new create]
 
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @review = Review.new(list: @list)
   end
 
   def new
@@ -15,15 +16,24 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path(@list)
+      redirect_to lists_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
 
   def list_params
     params.require(:list).permit(:name, :photo)
+  end
+
+  def find
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   root "lists#index"
   resources :lists, except: %i[edit update] do
     resources :bookmarks, only: %i[new create]
+    resources :reviews, only: :create
   end
 
   resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
